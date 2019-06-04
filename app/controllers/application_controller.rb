173e1ae79d2211ff076e0b8f6f88e48afc2f0ede
SedTheCursed <sed.robert@gmail.com>
2019-06-04 11:07:06 +0200

    first try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_action :get_footer_link
+
+  def get_footer_link
+    @socials = [
+      {
+        icon: "fab fa-facebook fa-2x",
+        link: "http://www.facebook.com/#"
+      },
+      {
+        icon: "fab fa-linkedin fa-2x",
+        link: "http://www.linkedin.com/#"
+      },
+      {
+        icon: "fab fa-twitter fa-2x",
+        link: "http://wwww.twitter.com/#"
+      },
+      {
+        icon: "fab fa-instagram fa-2x",
+        link: "http://www.instagram.com/#"
+      }
+    ]
+  end
 
 end
