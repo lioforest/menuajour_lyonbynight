d9f7a406544c60431a102008de63008594ab2592
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-06-04 16:22:19 +0200

    maj

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( team_photo_1.jpg )
+Rails.application.config.assets.precompile += %w( team_photo_3.jpg )
