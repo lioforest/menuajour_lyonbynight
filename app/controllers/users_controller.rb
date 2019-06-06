e17bccce556e282b34e2805e594fef8d8a1dad23
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-06-06 15:14:16 +0200

    routes fixed users_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 before_action :authenticate_user!, only: [:show]
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     @title = @user.email
   end
 
