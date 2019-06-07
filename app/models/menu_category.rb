1b3a78b76ce9ac8ce53e566d4c8eb1cca1ed548b
ragenchirm <noel.gagnaire@gmail.com>
2019-06-07 18:55:46 +0200

    validate for uniqness of orders inside menu and menu_category ok, autoincrement of order at the creation of menu_category and menu_items

--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -1,17 +1,91 @@
 class MenuCategory < ApplicationRecord
   validates :order, presence: true
+  validate :order_uniqueness_in_menu
+
+  before_validation :set_initial_order, on: :create
+
+
 
   belongs_to :menu
   belongs_to :category_type
 
-  has_many :items, source: :menu_items, class_name: "MenuItem", dependent: :destroy
+  has_many :menu_items, dependent: :destroy
 
-  def name
-    self.category_type.name
-  end
 
-  def add_item(_item_type, _order = 0)
-    MenuItem.create(item_type: _item_type, menu_category: self, order: _order)
+
+
+################################################
+################## Methods #####################
+################################################
+
+#***************** Public *********************#
+
+def name
+  self.category_type.name
+end
+
+def items
+ self.menu_items.order(:order)
+end
+
+def add_item(_item_type)
+  MenuItem.create(item_type: _item_type, menu_category: self, order: 0)
+end
+
+def add_item_by_name(_item_type_name)
+  MenuItem.create(menu_category: self, item_type: ItemType.where(name: _item_type_name)[0], order: 0)
+end
+
+def move_category_up
+  if self.order != get_smallest_order_in_menu
+
   end
+end
+
+#***************** Private *********************#
+
+private
+
+def get_highest_order_in_menu
+  highest_order = -1
+  MenuCategory.where(menu: self.menu).each {|_menu_category|
+    _menu_category_order = _menu_category.order
+    if _menu_category_order >= highest_order
+      highest_order= _menu_category_order
+    end
+  }
+  highest_order
+end
+
+def get_smallest_order_in_menu
+  smallest_order = get_highest_category_order_in_menu
+  MenuCategory.where(menu: self.menu).each {|_menu_category|
+    _menu_category_order = _menu_category.order
+    if _menu_category_order <= smallest_order
+      smallest_order= _menu_category_order
+    end
+  }
+  smallest_order
+end
+
+#******************* Initializers ************************#
+
+def set_initial_order
+  self.order = get_highest_order_in_menu + 1 if self.order.nil? || self.order <= get_highest_order_in_menu
+end
+
+#***************** Custom Validators *********************#
+
+def order_uniqueness_in_menu
+  menu_categories_orders =[]
+  MenuCategory.where(menu: self.menu).each {|_menu_category|
+    menu_categories_orders.append(_menu_category.order) if _menu_category != self
+  }
+
+  if menu_categories_orders.include?(self.order)
+    error_message = "menu_category.order (here #{self.order}) must unique inside a menu"
+    errors.add(:order, error_message)
+  end
+end
 
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,20 +1,62 @@
 class MenuItem < ApplicationRecord
   validates :order, presence: true
+  validate :order_uniqueness_in_menu_category
+
+  before_validation :set_initial_order, on: :create
 
   belongs_to :menu_category
   belongs_to :item_type
 
 
+################################################
+################## Methods #####################
+################################################
 
-  def name
-    self.item_type.name
+#***************** Public *********************#
+def name
+  self.item_type.name
+end
+
+def description
+  self.item_type.name
+end
+
+def price
+  self.item_type.name
+end
+
+  #***************** Private *********************#
+
+  private
+
+  def get_highest_order_in_menu_category
+    highest_order = -1
+    MenuItem.where(menu_category: self.menu_category).each {|_menu_item|
+      menu_item_order = _menu_item.order
+      if menu_item_order >= highest_order
+        highest_order = menu_item_order
+      end
+    }
+    highest_order
   end
 
-  def description
-    self.item_type.name
+  #******************* Initializers ************************#
+
+  def set_initial_order
+    self.order = get_highest_order_in_menu_category + 1 if self.order.nil? || self.order <= get_highest_order_in_menu_category
   end
 
-  def price
-    self.item_type.name
+#***************** Custom Validators *********************#
+
+def order_uniqueness_in_menu_category
+  menu_items_orders =[]
+  MenuItem.where(menu_category: self.menu_category).each {|_menu_item|
+    menu_items_orders.append(_menu_item.order) if _menu_item != self
+  }
+  if menu_items_orders.include?(self.order)
+    error_message = "menu_item.order (here #{self.order}) must unique inside a _category"
+    errors.add(:order, error_message)
   end
+end
+
 end
