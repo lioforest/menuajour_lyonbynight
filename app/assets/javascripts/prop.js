ddb01172a9843e50897fffb16bc23e2348104f22
SedTheCursed <sed.robert@gmail.com>
2019-06-07 17:55:26 +0200

    Template in show done

---
+++ app/assets/javascripts/prop.js
@@ -0,0 +1,5 @@
+totalWidth = () => 1920
+pageWidth = () => 1140
+pageHeight = () => 1632
+a4Width = () => 210
+a4Height = () => 297
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,11 +3,19 @@ class MenusController < ApplicationController
   end
 
   def show
+    @menu = get_menu(params[:id])
+    @title = @menu.title
   end
 
   def edit
   end
 
   def new
+  end
+  
+  private
+
+  def get_menu(id)
+    Menu.find(id)
   end
 end
