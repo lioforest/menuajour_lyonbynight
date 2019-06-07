ab9d9a8dde935522505f8f9813615eda791a8a68
Tahina93 <anne.tahina.metz@gmail.com>
2019-06-07 17:07:42 +0200

    unable to show image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.asset_host = "http://localhost:3000"
 
   config.action_mailer.delivery_method = :letter_opener
   #config.action_mailer.delivery_method = :mailjet
