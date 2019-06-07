dfd83be2cc5550dc3a62b92b07e13666020658cd
lioforest <lioforest@free.fr>
2019-06-07 15:53:30 +0200

    edit menu setup the page

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -6,6 +6,18 @@ class MenusController < ApplicationController
   end
 
   def edit
+  	@menu = Menu.find(params[:id])
+  end
+
+  def update
+    @menu = Menu.find(params[:id])
+    menu_params = params.require(:menu).permit(:name,:title,:subtitle)    
+    if @menu.update(menu_params)
+      render :edit
+
+    else
+      render :edit
+    end
   end
 
   def new
