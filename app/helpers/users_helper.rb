aca2b7a3f5fb38bb03d75f35b7eba4728ecc2fd4
lioforest <lioforest@free.fr>
2019-06-10 14:33:50 +0200

    add helper checked_user tested

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,13 @@
 module UsersHelper
 
+	def checked_user
+		@user_in_url = User.find(params[:user_id])
+	    if @user_in_url == current_user
+	    	return true
+	    else
+	    	return false
+	    end
+	end    
 
 
 
