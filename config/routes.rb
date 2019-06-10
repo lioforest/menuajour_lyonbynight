808bb5c5f7d5b320b8e88b229ba0601d310f3526
lioforest <lioforest@free.fr>
2019-06-10 19:13:19 +0200

    add form on page menu edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root "static#index"
   devise_for :users, path: "", :path_names => { :sign_in => 'se-connecter', :sign_out => 'se-deconnecter', :sign_up => 's-enregistrer' }
   scope(path_names: {new: "nouveau", edit: "modifier"}) do
+    
     resources :users, only: [:show], path: "clients" do
-      resources :menus
+        resources :menus
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
