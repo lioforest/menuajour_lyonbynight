f871917f347a690f14081bd90498927144a7af34
SedTheCursed <sed.robert@gmail.com>
2019-06-11 23:36:17 +0200

    Start the implementation of AJAX

--- app/controllers/item_types_controller.rb
+++ app/controllers/item_types_controller.rb
@@ -1,34 +1,49 @@
 class ItemTypesController < ApplicationController
  before_action :authenticate_user!
 
- def index
-  @category_types = current_user.category_types
-  @title = "Mes plats"
-end
+  def index
+    @category_types = current_user.category_types
+    @title = "Mes plats"
+  end
 
-def show
-end
+  def show
+  end
 
-def create
-  item_name = params[:name]
-  item_description = params[:description]
-  item_price = params[:price]
-  item_category_type = CategoryType.find(params[:category_type_id])
-  item_category_type.create_new_item_type(item_name, item_description, item_price)
-  redirect_back(fallback_location: root_path)
-end
+  def create
+    item_name = params[:name]
+    item_description = params[:description]
+    item_price = params[:price]
+    item_category_type = CategoryType.find(params[:category_type_id])
+    item_category_type.create_new_item_type(item_name, item_description, item_price)
+    redirect_back(fallback_location: root_path)
+  end
 
-def edit
-end
+  def edit
+    respond_to do | format |
+      format.html {}
+      format.js {}
+    end
+  end
 
-def update
-end
+  def update
+    if get_item.update(name: params[:name])
+      flash[:success] = 'Votre changement a été enregistré'
 
-def destroy
-  ItemType.find(params[:id]).destroy
-  redirect_back(fallback_location: root_path)
-end
+      render :edit
+    end
+  end
 
-def new
-end
+  def destroy
+    get_item.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  def new
+  end
+
+  private
+
+  def get_item
+    ItemType.find(params[:id])
+  end
 end
