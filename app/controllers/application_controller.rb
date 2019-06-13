ae9077120f861498d0fcfe6904bbb0c127ace888
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-06-13 13:30:03 +0200

    restrict subscribed user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -58,4 +58,14 @@ class ApplicationController < ActionController::Base
       end
   end
 
+  def is_subscribed_user?
+
+    if current_user.subscriptions.last != nil
+      if current_user.subscriptions.last.end_date > Date.today
+        return true
+      end
+  end
+    
+  end
+
 end
