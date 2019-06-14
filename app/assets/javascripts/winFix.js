ac6afdacd3a68d6ff7329b0ab6d5285b527ac18a
SedTheCursed <sed.robert@gmail.com>
2019-06-14 20:59:43 +0200

    add onload on winFix

--- app/assets/javascripts/winFix.js
+++ app/assets/javascripts/winFix.js
@@ -1,4 +1,8 @@
- if (/^Win/.test(navigator.platform)) {
-    document.querySelectorAll(".creation-subsection")
-        .forEach((div) => div.style.width = "99vw")
+function winFix() {
+    if (/^Win/.test(navigator.platform)) {
+        document.querySelectorAll(".creation-subsection")
+            .forEach((div) => div.style.width = "99vw")
+    }
 }
+
+window.onload = winFix()
